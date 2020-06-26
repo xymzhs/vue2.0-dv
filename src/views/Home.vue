1180015c7f00032260402a494cc3fe9937819f95
xymzhs <xymzhs@gmail.com>
2020-06-26 13:25:38 +0800

    Init

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+  .home {
+    width: 100%;
+    padding: 20px;
+    height: 100%;
+    box-sizing: border-box;
+    background: #eee;
+  }
+</style>
