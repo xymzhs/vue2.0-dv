3503577c7a975e71a039a93818c43db5590badc5
xymzhs <xymzhs@gmail.com>
2020-06-27 13:32:34 +0800

    Totalsales finished

--- src/components/TotalSales/index.vue
+++ src/components/TotalSales/index.vue
@@ -1,40 +1,45 @@
 <template>
-  <common-card
-    title="累计销售额"
-    value="$ 32,039,165"
-  >
-  
-  <template>
-      <div class="compare"></div>
-  </template>
-    <template v-slot:footer>
-        <div>
-            <span>昨日销售额</span>
-            <span class="money">$ 30,000,000</span>
+  <common-card title="累计销售额" value="$ 32,039,165">
+    <template>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="increase" />
         </div>
-        
+        <div class="compare">
+          <span>月同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="decrease" />
+        </div>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日销售额</span>
+      <span class="emphasis">$ 30,000,000</span>
     </template>
   </common-card>
 </template>
 
 <script>
-import commonCardMixin from "../../mixins/commonCardMixin"
+import commonCardMixin from "../../mixins/commonCardMixin";
 export default {
-    mixins: [commonCardMixin]
-}
+  mixins: [commonCardMixin]
+};
 </script>
 
-<style>
-    span{
-        font-size: 12px;
-    }
-    .compare{
-        height:100%;
-        background-color: yellow;
-    }
-    .money{
-        margin-left: 5px;
-        color: #333;
-        font-weight: 700;
-    }
+<style lang="scss" scoped>
+.compare-wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .compare {
+    margin-top: 3px;
+    font-size: 12px;
+    display: flex;
+    text-align: center;
+    color: #666;
+  }
+}
 </style>
